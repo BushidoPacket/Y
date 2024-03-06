b1effd660d34b3e12f2fce54d56b0d4651a0c7ca
Dominik Rotter <dominik.rotter.20@gmail.com>
2024-03-06 19:30:45 +0100

    login/register panel UI

--- Y-front-end/src/routes/Profile.jsx
+++ Y-front-end/src/routes/Profile.jsx
@@ -1,13 +1,82 @@
+import classes from "./Profile.module.css";
 
+import { useState } from "react";
 
 function Profile() {
-  return (
-    <>
-    <div>
-      <h1>Profile</h1>
-    </div>
-    </>
-  );
+  const [isLogin, setIsLogin] = useState(false);
+
+  const PasswordInput = ({ placeholder }) => {
+    const [password, setPassword] = useState("");
+
+    const handleChange = (event) => {
+      setPassword(event.target.value);
+    };
+
+    return (
+      <div className={classes.passwordInput}>
+        <input
+          type="password"
+          placeholder={placeholder}
+          value={password}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          placeholder={placeholder}
+          value={password}
+          onChange={handleChange}
+          className={classes.textOverlay}
+        />
+      </div>
+    );
+  };
+
+  const LoginPanel = () => {
+    return (
+      <>
+        <h1>Please login or register...</h1>
+        <div className={classes.profileContainer}>
+          <div className={classes.loginContainer}>
+            <h4>Login</h4>
+            <div className={classes.itemContainer}>
+              <label>Username</label>
+              <input type="text" placeholder="Username" />
+            </div>
+            <div className={classes.itemContainer}>
+              <label>Password</label>
+              <PasswordInput placeholder="Password" />
+            </div>
+            <button>Login</button>
+          </div>
+
+          <div className={classes.separator} />
+
+          <div className={classes.registerContainer}>
+            <h4>Register</h4>
+            <div className={classes.itemContainer}>
+              <label>Username</label>
+              <input type="text" placeholder="Username" />
+            </div>
+            <div className={classes.itemContainer}>
+              <label>E-mail</label>
+              <input type="email" placeholder="E-mail" />
+            </div>
+            <div className={classes.itemContainer}>
+              <label>Password</label>
+              <PasswordInput placeholder="Password" />
+            </div>
+            <div className={classes.itemContainer}>
+              <label>Repeat password</label>
+              <PasswordInput placeholder="Password" />
+            </div>
+            <button>Register</button>
+          </div>
+        </div>
+      </>
+    );
+  };
+
+  return <>{isLogin ? <h2>Settings</h2> : <LoginPanel />}</>;
 }
 
 export default Profile;
