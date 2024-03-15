e721415bcd675234533fdd874f1080a301b7efd0
Dominik Rotter <dominik.rotter.20@gmail.com>
2024-03-15 14:57:32 +0100

    added footer

---
+++ Y-front-end/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import classes from "./Footer.module.css";
+
+const Footer = () => {
+
+    return (
+        <>
+        <footer>
+            <p>The application was created as a credit project.</p>
+        </footer>
+        </>
+    )
+}
+
+export default Footer;
