86fabe37d4408e76cda608d62f01704fe9fb9735
Dominik Rotter <dominik.rotter.20@gmail.com>
2024-03-22 14:10:01 +0100

    login user module

---
+++ Y-front-end/src/components/Login.jsx
@@ -0,0 +1,56 @@
+import classes from "./Login.module.css";
+
+import API from "./Addressables";
+
+function Login() {
+
+    const loginUser = async (username, password) => {
+
+        const response = await fetch(`${API}/users/login`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
+        });
+
+        console.log(response);
+        if (response.status !== 200) {
+            const user = await response.json();
+            alert(user.error);
+        }
+
+        if (response.status === 200) {
+            const user = await response.json();
+            localStorage.setItem("token", user.token);
+            alert(user.message);
+            //redirect settings
+        }
+    };
+
+    const handleLoginSubmit = (event) => {
+        event.preventDefault();
+        const username = event.target.elements.username.value;
+        const password = event.target.elements.password.value;
+        loginUser(username, password);
+    };
+
+
+
+  return (
+    <>
+      <div className={classes.loginContainer}>
+        <form onSubmit={handleLoginSubmit}>
+          <h2>Login</h2>
+          <label htmlFor="username">Username:</label>
+          <input type="text" id="logUsername" name="username" required />
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="logPassword" name="password" required />
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    </>
+  );
+}
+
+export default Login;
