06f047be759816971e8a63ad4be86fb5396c3f96
Dominik Rotter <dominik.rotter.20@gmail.com>
2024-03-31 10:22:52 +0200

    created standalone component for search input and handling

---
+++ Y-front-end/src/components/SearchBar.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import classes from "./SearchBar.module.css";
+
+export default function SearchBar({ setParams }) {
+  const [author, setAuthor] = useState("");
+  const [text, setText] = useState("");
+
+  const handleAuthorChange = (e) => {
+    setAuthor(e.target.value);
+  };
+
+  const handleTextChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const handleButtonClick = () => {
+    setParams({ author, text });
+  };
+
+  return (
+    <>
+      <div className={classes.searchBarContainer}>
+        <input
+          onChange={handleAuthorChange}
+          type="text"
+          name="searchAuthor"
+          placeholder="Search by author..."
+        />
+        <input
+          onChange={handleTextChange}
+          type="text"
+          name="searchText"
+          placeholder="Search by text..."
+        />
+        <button onClick={handleButtonClick}>Search</button>
+      </div>
+    </>
+  );
+}
