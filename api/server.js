140e24f5dcbac26896d6acd9bd1e7ab6ab5f9cd9
Dominik Rotter <dominik.rotter.20@gmail.com>
2024-03-31 14:06:33 +0200

    edited posts get to reflex requests for searching, now works for both basic feed and search routes

--- api/server.js
+++ api/server.js
@@ -33,23 +33,27 @@ const dateFormat = (timestamp) => {
 };
 
 //Register a new user into DB
-//const hashPassword = require("./auth");
 app.post("/users/new", async (req, res) => {
   try {
-
     const { username, email, password } = req.body;
 
     const existingUser = await User.findOne({ username: username });
     if (existingUser) {
-      console.log("=== Somebody tried to register already existing username: " + username);
+      console.log(
+        "=== Somebody tried to register already existing username: " + username
+      );
       return res.status(400).json({ error: "Profile already exists." });
-    } 
+    }
 
     const existingEmail = await User.findOne({ email: email });
     if (existingEmail) {
-      console.log("=== Somebody tried to register already existing email: " + email);
-      return res.status(400).json({ error: "This e-mail address is already being used." });
-    } 
+      console.log(
+        "=== Somebody tried to register already existing email: " + email
+      );
+      return res
+        .status(400)
+        .json({ error: "This e-mail address is already being used." });
+    }
 
     const { saltValue, passwordValue } = hashPassword(password);
 
@@ -76,71 +80,107 @@ app.post("/users/new", async (req, res) => {
 });
 
 //Check whether an user is in DB with the right username and password
-//const compare = require("./auth");
 app.put("/users/login", async (req, res) => {
   const user = req.body.username;
   const password = req.body.password;
-  
 
   try {
-    const userFound = await User.findOne({username: user});
+    const userFound = await User.findOne({ username: user });
     if (userFound) {
+      const isPasswordMatch = compare(
+        password,
+        userFound.salt,
+        userFound.password
+      );
 
-      const isPasswordMatch = compare(password, userFound.salt, userFound.password);
-      
       if (isPasswordMatch) {
         const token = createToken(user, "30d");
-        res.status(200).json({ message: "User logged in successfully.", token });
+        res
+          .status(200)
+          .json({ message: "User logged in successfully.", token });
         console.log(
-          "=== User " + user + " has logged in at " + dateFormat(Date.now()) + "."
+          "=== User " +
+            user +
+            " has logged in at " +
+            dateFormat(Date.now()) +
+            "."
         );
       } else {
         res.status(401).json({ error: "Invalid password." });
         console.log(
-          "=== User " + user + " has tried to log in at " + dateFormat(Date.now()) + " with invalid password."
+          "=== User " +
+            user +
+            " has tried to log in at " +
+            dateFormat(Date.now()) +
+            " with invalid password."
         );
-      };
-
+      }
     } else {
-        res.status(404).json({ error: "User not found." });
-        console.log(
-          "=== User " + user + " has tried to log in at " + dateFormat(Date.now()) + ", but user was not found."
-        );
+      res.status(404).json({ error: "User not found." });
+      console.log(
+        "=== User " +
+          user +
+          " has tried to log in at " +
+          dateFormat(Date.now()) +
+          ", but user was not found."
+      );
     }
-    
   } catch (error) {
     console.error(error);
     res.status(500).json({
       error:
-        "There has been an error while logging in an user " + user + " . Please try again later."
+        "There has been an error while logging in an user " +
+        user +
+        " . Please try again later.",
     });
   }
 });
 
-
-/*app.put("/users/verify", async (req, res) => {
+app.put("/users/info", async (req, res) => {
   const token = req.body.token;
 
   try {
     const decoded = await verifyToken(token);
     //console.log("=== Token verified for user " + decoded.user + " .");
-    res.status(200).json({ message: "Token verified.", user: decoded.user });
+    const userFound = await User.findOne({ username: decoded.user });
+    const { username, email, creationDate, profilePicture } = userFound;
+
+    res.status(200).json({
+      message: "Token verified.",
+      username: username,
+      email: email,
+      creationDate: creationDate,
+      profilePicture: profilePicture,
+    });
+
+    console.log(
+      "=== User " +
+        decoded.user +
+        " info fetched at " +
+        dateFormat(Date.now()) +
+        "."
+    );
   } catch (error) {
     console.log("=== Token not verified.");
     res.status(401).json({ error: "Token not verified." });
   }
-});*/
-
+});
 
 //Publish a new post into DB
 app.post("/posts/new", async (req, res) => {
-
   //Authorize user
   const token = req.headers["authorization"];
 
   let author = "";
 
-  if (!token || token === "" || token === "null" || token === "undefined" || token === undefined || token === null) {
+  if (
+    !token ||
+    token === "" ||
+    token === "null" ||
+    token === "undefined" ||
+    token === undefined ||
+    token === null
+  ) {
     return res.status(401).json({ error: "Unauthorized." });
   }
 
@@ -164,7 +204,13 @@ app.post("/posts/new", async (req, res) => {
     await newPost.save();
 
     res.status(201).json(newPost);
-    console.log("=== New post created at " + dateFormat(Date.now()) + " from " + author + ".");
+    console.log(
+      "=== New post created at " +
+        dateFormat(Date.now()) +
+        " from " +
+        author +
+        "."
+    );
   } catch (error) {
     console.error(error);
     res.status(500).json({
@@ -181,15 +227,23 @@ app.get("/posts", async (req, res) => {
   const skipAmount = (page - 1) * limit;
   const order = parseInt(req.query.order) || -1;
 
+  let params = {};
+  if (req.query.author && req.query.author !== "" && req.query.author !== "null" && req.query.author !== "undefined" && req.query.author !== undefined && req.query.author !== null) {
+    params.author = { $regex: new RegExp(req.query.author), $options: 'i' };
+  }
+  if (req.query.text && req.query.text !== "" && req.query.text !== "null" && req.query.text !== "undefined" && req.query.text !== undefined && req.query.text !== null) {
+    params.text = { $regex: new RegExp(req.query.text), $options: 'i' };
+  }
+
   try {
-    const posts = await Post.find()
+    const posts = await Post.find(params)
       .sort({ timestamp: order })
       .skip(skipAmount)
       .limit(limit);
     //await new Promise((resolve) => setTimeout(resolve, 2000));
     res.status(200).json(posts);
     console.log(
-      "=== Posts fetched successfully at " + dateFormat(Date.now()) + "."
+      "=== Posts fetched, page " + page + " at " + dateFormat(Date.now()) + "."
     );
   } catch (error) {
     console.error(error);
@@ -201,9 +255,32 @@ app.get("/posts", async (req, res) => {
 });
 
 //Publish a new comment into DB
-app.post("/comments", async (req, res) => {
+app.post("/comments/new", async (req, res) => {
+  //Authorize user
+  const token = req.headers["authorization"];
+
+  let author = "";
+
+  if (
+    !token ||
+    token === "" ||
+    token === "null" ||
+    token === "undefined" ||
+    token === undefined ||
+    token === null
+  ) {
+    return res.status(401).json({ error: "Unauthorized." });
+  }
+
   try {
-    const { author, text, postParentID } = req.body;
+    const decoded = await verifyToken(token);
+    author = decoded.user;
+  } catch (error) {
+    res.status(401).json({ error: "Invalid token." });
+  }
+
+  try {
+    const { text, postParentID } = req.body;
 
     const newComment = new Comment({
       author,
@@ -228,7 +305,7 @@ app.post("/comments", async (req, res) => {
 //Fetch all comments from DB with arguments, from a specific post
 app.get("/comments", async (req, res) => {
   const page = parseInt(req.query.page) || 1;
-  const limit = parseInt(req.query.limit) || 10;
+  const limit = parseInt(req.query.limit) || 5;
   const skipAmount = (page - 1) * limit;
   const order = parseInt(req.query.order) || -1;
   const postID = req.query.postParentID;
@@ -238,11 +315,11 @@ app.get("/comments", async (req, res) => {
       .sort({ timestamp: order })
       .skip(skipAmount)
       .limit(limit);
-
+    //await new Promise((resolve) => setTimeout(resolve, 2000));
     res.status(200).json(comments);
-    console.log(
+    /*console.log(
       "=== Comments from post ID " + postID + " fetched successfully."
-    );
+    );*/
   } catch (error) {
     console.error(error);
     res.status(500).json({
